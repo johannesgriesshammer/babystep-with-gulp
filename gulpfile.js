ce75591d94d89a0f9b27ac7fde86a5e563e1b898
Johannes Grießhammer <18589999+johannesgriesshammer@users.noreply.github.com>
2017-11-02 18:24:42 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-/*
-What you could add to the sass task:
-- Autoprefixer
-- (Better) error handling
-- Compress file
-- rename output file after compression to xyz.min.css
-- implement browserify to reload on changes
-*/
-
 gulp.task('sass', function() {
     //the source. Where is the stuff to do something with?
     return gulp.src('./scss/**/*.scss')
