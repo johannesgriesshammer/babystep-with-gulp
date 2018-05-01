8e17645bfb4afcf84cadf134bc0fb0ee9885e42d
Johannes Grießhammer <18589999+johannesgriesshammer@users.noreply.github.com>
2018-05-01 20:53:02 +0200

    Sass Watch Task added

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
@@ -5,7 +7,11 @@ gulp.task('sass', function() {
     //the source. Where is the stuff to do something with?
     return gulp.src('./scss/**/*.scss')
     //the actual thing(s) to do.
-    .pipe(sass())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     //the target. Where to put new stuff after thing(s) have been done.
     .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/**/*.scss', ['sass']);
 });
